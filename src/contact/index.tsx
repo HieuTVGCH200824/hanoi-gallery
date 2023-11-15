27c792f200f8ad3e591f0d74ef209210812eacf4
HieuTVGCH200824 <hieutvgch200824@fpt.edu.vn>
2023-11-15 23:54:10 +0700

    add member name

--- src/contact/index.tsx
+++ src/contact/index.tsx
@@ -2,18 +2,22 @@ const pple = [
   {
     ava: "/public/hand-ava.jpg",
     fb: "https://www.facebook.com/mhte.hin007",
+    name: "Trần Minh Hạnh",
   },
   {
     ava: "/public/linh-ava.jpg",
     fb: "https://www.facebook.com/linh.phanthuy.399",
+    name: "Phan Thùy Linh",
   },
   {
     ava: "/public/hieu-ava.jpg",
     fb: "https://www.facebook.com/htv912/",
+    name: "Trần Văn Hiếu",
   },
   {
     ava: "/public/hiep-ava.jpg",
     fb: "https://www.facebook.com/hiepdyu.03",
+    name: "Nguyễn Duy Hiệp",
   },
 ];
 
@@ -23,16 +27,19 @@ const Contact = () => {
       <h1 className="text-center mt-40 font-playfair text-4xl font-bold text-[#001ECB] ">
         Thành viên
       </h1>
-      <div className="grid grid-cols-2 gap-10 mx-auto mt-20">
+      <div className="grid grid-cols-4 gap-10 mx-auto mt-20">
         {pple.map((p) => (
-          <a
-            href={p.fb}
-            target="_blank"
-            className="rounded-full border-[#001ECB] border w-40 justify-self-center"
-            key={p.fb}
-          >
-            <img src={p.ava} className="rounded-full" alt="" />
-          </a>
+          <div className="flex flex-col items-center">
+            <a
+              href={p.fb}
+              target="_blank"
+              className="rounded-full border-[#001ECB] border w-40 justify-self-center"
+              key={p.fb}
+            >
+              <img src={p.ava} className="rounded-full" alt="" />
+            </a>
+            <span className="mt-2">{p.name}</span>
+          </div>
         ))}
       </div>
     </div>
