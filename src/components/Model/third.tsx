d9ecdb363df9ba4397e4cb7a6baf13e225b7e92a
HieuTVGCH200824 <hieutvgch200824@fpt.edu.vn>
2023-11-16 09:48:55 +0700

    add third model loading screen

--- src/components/Model/third.tsx
+++ src/components/Model/third.tsx
@@ -1,8 +1,8 @@
 import { Canvas } from "@react-three/fiber";
 import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { useRef } from "react";
-import { Environment, OrbitControls } from "@react-three/drei";
+import { useRef, Suspense } from "react";
+import { Environment, OrbitControls, Loader } from "@react-three/drei";
 
 const Setup = () => {
   const gltf = useLoader(GLTFLoader, "/chua-mot-cot.glb");
@@ -17,17 +17,22 @@ const Setup = () => {
 
 const Model = () => {
   return (
-    <Canvas camera={{ position: [-15, 10, 15], fov: 25 }}>
-      <ambientLight intensity={0.3} />
-      <directionalLight color="white" position={[0, 0, 5]} />
-      <Environment preset="sunset" />
-      <OrbitControls
-        makeDefault
-        minPolarAngle={1}
-        maxPolarAngle={Math.PI - Math.PI / 2}
-      />
-      <Setup />
-    </Canvas>
+    <>
+      <Canvas camera={{ position: [15, 10, -15], fov: 25 }}>
+        <ambientLight intensity={0.3} />
+        <directionalLight color="white" position={[0, 0, 5]} />
+        <Environment preset="sunset" />
+        <OrbitControls
+          makeDefault
+          minPolarAngle={1}
+          maxPolarAngle={Math.PI - Math.PI / 2}
+        />
+        <Suspense fallback={null}>
+          <Setup />
+        </Suspense>
+      </Canvas>
+      <Loader />
+    </>
   );
 };
 
